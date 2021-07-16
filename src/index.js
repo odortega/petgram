05fcbf03c70e9fcdc659aac1535170525d277288
odortega <odortega@gmail.com>
2021-07-16 01:48:03 -0500

    [add] initial commit

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import ApolloClient from 'apollo-boost'
+import { ApolloProvider } from 'react-apollo'
+import Context from './Context'
+
+import { App } from './App'
+
+const client = new ApolloClient({
+  uri: 'https://petgram-server.midudev.now.sh/graphql'
+})
+
+ReactDOM.render(
+  <Context.Provider>
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
+  </Context.Provider>,
+  document.getElementById('app'))
